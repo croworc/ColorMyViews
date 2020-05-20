d21096c3dc55d650c90cd058736bbff5b0f68e51
ikrotzky <ikr@croworc.com>
2020-05-20 16:13:04 +0200

    feat: Exchange color of box five to bright blue

--- app/src/main/java/com/croworc/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/croworc/android/colormyviews/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text  -> view.setBackgroundResource(android.R.color.holo_green_dark)
-            R.id.box_five_text  -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_five_text  -> view.setBackgroundResource(android.R.color.holo_blue_bright)
 
             // Buttons using custom colors for background
             R.id.red_button    -> view.setBackgroundResource(R.color.my_red)
